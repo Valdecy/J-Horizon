82b358843965eeede2685320ad7f5023daa0b8fb
Valdecy Pereira <valdecypereira@yahoo.com.br>
2020-05-20 10:05:25 -0300

    Update Util_Manhattan.java

--- guiHorizon/Util_Manhattan.java
+++ guiHorizon/Util_Manhattan.java
@@ -4,7 +4,7 @@ package guiHorizon;
 
 //This file is part of J-Horizon.
 //
-//J-ELECTRE is free software: you can redistribute it and/or modify
+//J-Horizon is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
 //the Free Software Foundation, either version 3 of the License, or
 //(at your option) any later version.
